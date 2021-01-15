24e52472c54ffe98a82bf36e894b682a783facc9
Arthur Aslanyan <arthur.e.aslanyan@gmail.com>
2021-01-15 23:15:36 +0600

    Argsparse to save data
    - [ADDED] Mutual exclusive group - delete and person
    - [ADDED] Saving people to db
    - [ADDED] days var with lowered days
    - [CHANGED] Moved old processing code to a different file

--- loader.py
+++ loader.py
@@ -1,61 +1,6 @@
 import json
 import sys
-from calendar import day_name
-from datetime import time
 from json.decoder import JSONDecodeError
-
-
-class Processor:
-
-    def __init__(self, people=[]) -> None:
-        self.people = people
-
-    def print_people(self):
-        print('People in the db.json:')
-        for person in self.people:
-            print(person)
-
-    def input_times(self, schedule: list):
-        while True:
-            print('\tFrom? (Enter to skip) ', end='')
-            from_time = input()
-
-            if not from_time:
-                break
-
-            from_time = time.fromisoformat(from_time)
-
-            to_time = ''
-            while not to_time:
-                print('\tTo? ', end='')
-                to_time = input()
-                print()
-
-            to_time = time.fromisoformat(to_time)
-
-            schedule.append({'from': str(from_time), 'to': str(to_time)})
-
-    def input_schedule(self, person: dict):
-        schedule = {}
-        for (key, day) in enumerate(day_name):
-            print(f'\tSchedule of {person["name"]} for {day}:')
-            schedule[key] = []
-            self.input_times(schedule[key])
-        person['schedule'] = schedule
-
-    def input_people(self, people: list):
-        while True:
-            print('Person name? (Enter to skip) ', end='')
-            name = input()
-
-            if not name:
-                break
-
-            person = {'name': name}
-            self.input_schedule(person)
-
-            people.append(person)
-
 
 class Source:
     def __init__(self, path: str) -> None:
--- main.py
+++ main.py
@@ -10,6 +10,8 @@ import pprint
 #locale.setlocale(locale.LC_ALL, '')
 # TODO: Possible bug - I want days in english, what happens if I don't set locale?
 
+days = tuple([d.lower() for d in calendar.day_abbr])
+
 parser = argparse.ArgumentParser(description='Tool to figure out sum of busy schedules from different people for a week')
 
 parser.add_argument('db', help='Path to a json file with data')
@@ -17,18 +19,52 @@ parser.add_argument('-l', '--list', help='Show current list of people in db', ac
 
 parser.add_argument('-s', '--show', help='Show busy schedule for people', nargs='+', metavar='NAME')
 
-time_group = parser.add_argument_group('time editing')
-time_group.add_argument('-p', '--person', help='Add new person or edit an existing one in the db')
+exclusive_group = parser.add_mutually_exclusive_group()
 
-for key, day in enumerate(calendar.day_abbr):
-    time_group.add_argument(f'--{day.lower()}', help=f'Busy time range for {calendar.day_name[key]}', nargs=2, action='append', metavar=('FROM', 'TO'))
+exclusive_group.add_argument('-d', '--delete', help='Delete a person from db')
+exclusive_group.add_argument('-p', '--person', help='Add new person or edit an existing one in the db')
+
+time_group = parser.add_argument_group('time editing')
+for key, day in enumerate(days):
+    time_group.add_argument(f'--{day}', help=f'Busy time range for {calendar.day_name[key]}', nargs=2, action='append', metavar=('FROM', 'TO'))
 
 args = parser.parse_args()
 
 source = Source(args.db)
-data = source.load()
+people = source.load()
 
 if args.list:
-    pprint.pprint(data)
+    pprint.pprint(people)
 
-print(args)
+if args.delete:
+    for key, person in enumerate(people):
+        if person['name'] == args.delete:
+            del people[key]
+            source.save(people)
+            print('Deleted!')
+            break
+    else:
+        print('Not found')
+
+
+if args.person:
+    new_person = {'name': args.person, 'schedule': {}}
+    for key, day in enumerate(days):
+        try:
+            new_person['schedule'][key] = getattr(args, day)
+        except AttributeError:
+            continue
+
+    for key, person in enumerate(people):
+        if person['name'] == new_person['name']:
+            people[key] = new_person
+            break
+    else:
+        people.append(new_person)
+
+
+    source.save(people)
+    pprint.pprint(people)
+    print('Saved!')
+
+
